de5193ee3063768cc24276b70b81ed05af071825
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2023-07-18 15:51:19 -0400

    and add turf

---
+++ turf.py
@@ -0,0 +1,58 @@
+from serialcobsdevice import SerialCOBSDevice
+from enum import Enum
+from bf import bf
+from dev_submod import dev_submod
+from pueo_turfctl import PueoTURFCTL
+
+class PueoTURF:
+    class DateVersion:
+        def __init__(self, val):
+            self.major = (val >> 12) & 0xF
+            self.minor = (val >> 8) & 0xF
+            self.rev = (val & 0xFF)
+            self.day = (val >> 16) & 0x1F
+            self.mon = (val >> 21) & 0xF
+            self.year = (val >> 25) & 0x7F
+
+        def __str__(self):
+            return f'v{self.major}.{self.minor}.{self.rev} {self.mon}/{self.day}/{self.year}'
+
+        def __repr__(self):
+            val = (self.year << 25) | (self.mon << 21) | (self.day << 16) | (self.major<<12) | (self.minor<<8) | self.rev
+            return f'TURF.DateVersion({val})'
+
+    map = { 'FPGA_ID' : 0x0,
+            'FPGA_DATEVERSION' : 0x4,
+            'DNA' : 0x8 }
+
+    class AccessType(Enum):
+        SERIAL = 'Serial'
+        ETH = 'Ethernet'
+        HSK = 'Housekeeping'
+    
+    def __init__(self, accessInfo, type=AccessType.SERIAL):
+        if type == self.AccessType.SERIAL:
+            self.dev = SerialCOBSDevice(accessInfo, 115200)
+            self.reset = self.dev.reset
+            self.read = self.dev.read
+            self.write = self.dev.write
+            self.writeto = self.dev.writeto
+            self.dev.reset()
+        self.ctl = PueoTURFCTL(self.dev, 0x10000)
+
+    def identify(self):
+        def str4(num):
+            id = str(chr((num>>24)&0xFF))
+            id += chr((num>>16)&0xFF)
+            id += chr((num>>8)&0xFF)
+            id += chr(num & 0xFF)
+            return id
+
+        fid = str4(self.read(self.map['FPGA_ID']))
+        print("FPGA:", fid, end=' ')
+        if fid == "TURF":
+            fdv = self.DateVersion(self.read(self.map['FPGA_DATEVERSION']))
+            print(fdv)
+        else:
+            print('')
+            
