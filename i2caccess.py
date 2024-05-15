4dd1ce0c4fd155b0d9db0facb69bd1197cce5608
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-05-15 15:09:05 -0400

    ensure stop actually happens + add SMBus read command

--- i2caccess.py
+++ i2caccess.py
@@ -13,71 +13,72 @@
 
 class I2cAccess(object):
     def __init__(self, scl, sda):
-        self.__sda = sda
-        self.__scl = scl
+        self.sda = sda
+        self.scl = scl
 
-        self.__sda.hiz()
-        self.__scl.hiz()
+        self.sda.hiz()
+        self.scl.hiz()
     
     def wait(self):
         "Delay for reliable I2C operation. May be too small for Pi type 2"
         return
 
     def start(self):
-        self.__sda.hiz()
-        self.__scl.hiz()
+        self.sda.hiz()
+        self.scl.hiz()
         self.wait()
-        self.__sda.lo()
+        self.sda.lo()
         self.wait()
-        self.__scl.lo()
+        self.scl.lo()
         self.wait()
         self.wait()
         return
 
     def clock(self):
         self.wait()
-        self.__scl.hiz()
+        self.scl.hiz()
         self.wait()
-        self.__scl.lo()
+        self.scl.lo()
         return
 
     def rxAck(self):        
-        self.__scl.lo()
-        self.__sda.hiz()
-        self.__scl.hiz()
+        self.scl.lo()
+        self.sda.hiz()
+        self.scl.hiz()
         self.wait()
-        return False if self.__sda.value() == 1 else True
-
+        return False if self.sda.value() == 1 else True
+    
     def txAck(self):
-        self.__sda.lo()
+        self.sda.lo()
         self.wait()
         self.clock()
-        self.__sda.hiz()
+        self.sda.hiz()
         return
-
+    
     def stop(self):
-        self.__sda.lo()
-        self.__scl.hiz()
+        self.scl.lo()
+        self.sda.lo()
+        self.scl.hiz()
         self.wait()
-        self.__sda.hiz()
+        self.sda.hiz()
         return
 
     def rxBit(self):
         bit = 0
         self.wait()
-        self.__scl.hiz()
+        self.scl.hiz()
         self.wait()
-        bit = self.__sda.value()
+        bit = self.sda.value()
         self.wait()
-        self.__scl.lo()
+        self.scl.lo()
         self.wait()
         return bit
 
     def rxByte(self):
         byte = 0
-        self.__scl.lo()
+        self.scl.lo()
         self.wait()
-        self.__sda.hiz()
+        self.sda.hiz()
         for i in range(0, 8):
             byte = (byte << 1) + self.rxBit()
         return byte
@@ -85,17 +86,17 @@ class I2cAccess(object):
     def txByte(self, byte):
         self.__byte = byte
         self.__count = 0
-        self.__scl.lo()
-        self.__sda.lo()
+        self.scl.lo()
+        self.sda.lo()
         self.wait()
         for self.__count in range(0, 8):
             if (self.__byte & 0x80) == 0:
-                self.__sda.lo()
+                self.sda.lo()
             else:
-                self.__sda.hiz()
+                self.sda.hiz()
             self.__byte = self.__byte << 1
             self.clock()
-        self.__sda.hiz()
+        self.sda.hiz()
         return self.rxAck()
 
     # returns True on error, False if not
@@ -121,12 +122,51 @@ class I2cAccess(object):
         self.stop()
         return False
 
+    # read using a restart
+    def readFrom(self, address, regAddr, nbytes=1):
+        self.start()
+        addr = (address << 1) 
+        ack = self.txByte(addr)
+        if not ack:
+            print("no acknowledge at addr")
+            self.stop()
+            return (True, None)
+        ack = self.txByte(regAddr)
+        if not ack:
+            print("no acknowledge at regAddr")
+            self.stop()
+            return (True, None)
+        # repeated start
+        self.scl.lo()
+        self.scl.hiz()
+        self.sda.lo()
+        addr = (address << 1) | 1
+        ack = self.txByte(addr)
+        if not ack:
+            print("no acknowledge at read addr")
+            self.stop()
+            return (True, None)
+        rxd = []
+        if nbytes == 0:
+            self.stop()
+            return (False, None)
+        while nbytes > 0:
+            rxd.append(self.rxByte())
+            nbytes = nbytes - 1
+            if nbytes:
+                self.txAck()
+            else:
+                self.clock()
+        self.stop()
+        return (False, rxd)
+    
     def read(self, address, nbytes=1):
         self.start()
         addr = (address << 1) | 1
         ack = self.txByte(addr)
         if not ack:
             print("no acknowledge at addr")
+            self.stop()
             return (True, None)
         rxd = []
         if nbytes == 0:
