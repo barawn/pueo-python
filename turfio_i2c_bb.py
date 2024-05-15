a713dd9aa84165fe4b4eb960e96ec4f5e476fff8
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-05-15 11:53:57 -0400

    rename i2c access

--- turfio_i2c_bb.py
+++ turfio_i2c_bb.py
@@ -1,9 +1,9 @@
 from genshift import GenShiftGPIO
-from universal-i2c-bitbang import I2CAccess
+from i2caccess import I2cAccess
 
 class PueoTURFIOI2C:
     def __init__(self, dev):
-        self.i2c = I2CAccess(scl=GenShiftGPIO(dev,6),
+        self.i2c = I2cAccess(scl=GenShiftGPIO(dev,6),
                              sda=GenShiftGPIO(dev,5))
         self.write = self.i2c.write
         self.read = self.i2c.read
