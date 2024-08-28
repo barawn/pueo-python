4bdb9abe1ca75aa96ebef1fcc05af94d86e4576c
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-08-28 16:02:43 -0400

    allow check_eye to work on an 8-bit interface

--- pueo_utils.py
+++ pueo_utils.py
@@ -1,4 +1,8 @@
 # General common PUEO utility functions
+from bf import bf
+
+train32 = 0xA55A6996
+train8 = 0x6A
 
 # This returns either None (incorrect eye value)
 # or the number of bit-slips required to match up.
@@ -10,12 +14,12 @@
 # 0xD4AB4D32 (3 bitslips needed)
 # Note that we ALSO need to check all the nybble-rotated
 # versions of this
-def check_eye(eye_val):
-    trainValue = 0xA55A6996
+def check_eye(eye_val, bw=32, trainValue=train32):
     testVal = int(eye_val)
     def rightRotate(n, d):
-        return (n>>d)|(n<<(32-d)) & 0xFFFFFFFF
-    for i in range(32):
+        return (n>>d)|(n<<(bw-d)) & (2 ** bw - 1)
+    for i in range(bw):
         if testVal == rightRotate(trainValue, i):
             return i
     return None
+
