c07746f51f1d6ea3e8012f7295f494b5b2800dc8
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-10-30 16:48:51 -0400

    do everything relative to our script location

--- make_surf.sh
+++ make_surf.sh
@@ -2,6 +2,8 @@
 # this script builds the SURF portion of pueo-python
 # in a target.
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 COMMON_FILES="bf.py \
 	      dev_submod.py \
 	      wbspi.py \
@@ -18,7 +20,7 @@ DEST=$1
 mkdir -p $DEST/pueo/surf
 mkdir -p $DEST/pueo/common
 
-cp pueo/surf/*.py $DEST/pueo/surf/
-cp pueo/common/__init__.py $DEST/pueo/common/
+cp ${SCRIPT_DIR}/pueo/surf/*.py $DEST/pueo/surf/
+cp ${SCRIPT_DIR}/pueo/common/__init__.py $DEST/pueo/common/
 for p in ${COMMON_FILES} ; do
-    cp pueo/common/$p $DEST/pueo/common/
+    cp ${SCRIPT_DIR}/pueo/common/$p $DEST/pueo/common/
