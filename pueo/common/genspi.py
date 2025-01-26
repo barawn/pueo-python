dac228a3d3273f7acf13716dd6d74532c16fdb80
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-01-26 11:57:13 -0500

    add stuff for spi

---
+++ pueo/common/genspi.py
@@ -0,0 +1,47 @@
+# wrapper for a SPIFlash object using
+# the GenShift module
+# the SPIFlash object expects
+# a 'command' which takes 
+#
+# command( val, num_dummy_bytes, num_read_bytes, data_in_bytes )
+# genshift gives us
+# shift(self, val, auxVal=0, bitOrder=BitOrder.LSB_FIRST, numBits=8)
+# so it's pretty much just multiple shift commands
+#
+# this is going to end up being pretty slow bc without the
+# magic speedup provided by bursts since we're sending 4x the data
+# note you need to do enable() first
+class GenSPI:
+    def __init__(self, genshift, ifnum, cspin, prescale=0):
+        self.dev = genshift
+        self.shift = self.dev.shift
+
+        high = genshift.GpioState.GPIO_HIGH
+        low = genshift.GpioState.GPIO_LOW
+        self.chipselect = lambda v : self.dev.gpio(cspin,
+                                                   high if v else low)
+        en = self.dev.enable
+        dis = self.dev.disable
+        
+        self.enable = lambda v : en(ifnum, prescale) if (v) else dis()        
+
+    def command(self,
+                val,
+                num_dummy_bytes, num_read_bytes, data_in_bytes ):
+        order = self.dev.BitOrder.MSB_FIRST
+        self.chipselect(True)
+        # first send command
+        self.dev.shift(val, bitorder=order)
+        # next send data_in_bytes
+        for b in data_in_bytes:
+            self.dev.shift(b, bitorder=order)
+        # next send dummy bytes
+        for d in range(num_dummy_bytes):
+            self.dev.shift(0x00, bitorder=order)
+        # next send bytes to read
+        r = []
+        for r in range(num_read_bytes):
+            r.append(self.dev.shift(0x00, bitorder=order))
+        self.chipselect(False)
+        return r
+    
