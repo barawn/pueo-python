0589d928cf2eb7f1ee9739a7f4d2293efcc6f331
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-02-21 12:15:04 -0500

    let's give make_turf.sh a whirl

---
+++ make_turf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# for now just friggin grab everything
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASE_SOURCE[0]}" )" &> /dev/null && pwd )
+
+DEST=$1
+
+mkdir -p $DEST/pueo/surf
+mkdir -p $DEST/pueo/turf
+mkdir -p $DEST/pueo/turfio
+mkdir -p $DEST/pueo/common
+
+cp ${SCRIPT_DIR}/pueo/surf/*.py $DEST/pueo/surf/
+cp ${SCRIPT_DIR}/pueo/turf/*.py $DEST/pueo/turf/
+cp ${SCRIPT_DIR}/pueo/turfio/*.py $DEST/pueo/turfio/
+# common has subdirs, no one else does
+cp ${SCRIPT_DIR}/pueo/common/*.py $DEST/pueo/common/
+for i in `cd ${SCRIPT_DIR}/pueo/common ; ls -d */ | grep -v __pycache__`
+do
+    mkdir $DEST/pueo/common/$i
+    cp ${SCRIPT_DIR}/pueo/common/${i}*.py ${DEST}/pueo/common/$i
+done
+
