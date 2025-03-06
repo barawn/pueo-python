402d001065e932892708d130300431a51a4a8d29
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-03-06 16:18:25 -0500

    more debugging

--- pueo/eth/turfeth.py
+++ pueo/eth/turfeth.py
@@ -3,6 +3,18 @@ import time
 import struct
 import ipaddress
 
+# NOTE:
+#
+# The Ethernet UDP stuff presents 64-bit data in a
+# LITTLE-ENDIAN fashion. As in, the first byte we
+# receive is [7:0], the next is [15:8], etc.
+#
+# This means when you send stuff you need to make sure
+# it's little endian, rather than network byte order.
+#
+# In Python we just get bytes from the socket
+# functions, so we can slice & dice them ourselves.
+#
 class TURFEth:
     """ helper class for TURF ethernet testing """
 
@@ -42,4 +54,21 @@ class TURFEth:
         return resp[2:].hex(sep=':')
         
     def read(self, addr):
+        # encode the read directly - we don't need to swap it below
+        d = addr.to_bytes(3, 'little') + self.tag.to_bytes(1, 'little')
+
+        # this is the Be Bold method
+        # the "correct" method here is to create a separate process/thread
+        # which handles the send/receive/repeat request if lost method.
+        # We DO NOT have to use the cs port here, the read/write guys
+        # respond to whatever port sent them data.
+        # So it probably makes sense to completely farm off the event
+        # stuff.
+
+        # we do NOT need to reverse bytes here
+        self.cs.sendto( msg, (str(self.turf_ip), self.turf_rdp))
+        data, addr = self.cs.recvfrom(1024)
+        resp = data[::-1]
+        # this part is bullcrap for now
+        print("Response:", resp.hex(sep=' '))
         
