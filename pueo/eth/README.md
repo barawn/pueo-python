85e66afc66271f0ae09f21913643e0da4961f5a3
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-03-06 11:45:51 -0500

    ethernet testing stuff

---
+++ pueo/eth/README.md
@@ -0,0 +1,5 @@
+# Ethernet tools
+
+This stuff is temporary, it exists to debug the TURF ethernet interface
+until enough of it is solid that the register access paths can be
+integrated and then as a reference for SFC stuff.
---
+++ pueo/eth/turfeth.py
@@ -0,0 +1,32 @@
+import socket
+import time
+import struct
+import ipaddress
+
+class TURFEth:
+    """ helper class for TURF ethernet testing """
+
+    # these ports are fixed
+    UDP_CTRL = 21603
+    
+    def __init__(self,
+                 turf = "192.168.1.28",
+                 cs_ip = "192.168.1.1",
+                 cs_port = 21347)
+        """ no event path right now """
+        self.cs_ip = ipaddress.ip_address(cs_ip)
+        self.cs_port = cs_port
+        self.turf_ip = ipaddress.ip_address(turf)
+        self.turf_port = self.UDP_CTRL
+        
+        self.cs = socket.socket(socket.AF_INET,
+                                socket.SOCK_DGRAM)
+        self.cs.bind( (str(cs_ip), cs_port) )
+
+    def ctrl_identify(self):
+        msg = b'\x00'*6+b'ID'
+        self.cs.sendto( msg, (str(self.turf_ip), self.turf_port))
+        data, addr = self.cs.recvfrom(1024)
+        resp = bytearray(data).reverse()
+        print("Response: ", resp.hex())
+        
