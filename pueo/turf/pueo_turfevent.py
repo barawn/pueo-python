b5d945e62a9e85f8d3848ce4ebb7c6fbba23da46
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-04-24 17:21:10 -0400

    add the turf event submodule

---
+++ pueo/turf/pueo_turfevent.py
@@ -0,0 +1,42 @@
+from ..common.bf import bf
+from ..common.dev_submod import dev_submod
+
+class PueoTURFEvent(dev_submod):
+    """ Event control and statistics core """
+    map = { 'CTRL' : 0x0,
+            'NDWORDS0' : 0x10,
+            'NDWORDS1' : 0x14,
+            'NDWORDS2' : 0x18,
+            'NDWORDS3' : 0x1C }
+
+    def __init__(self, dev, base):
+        super().__init__(dev, base)
+
+    def statistics(self, verbose=True):
+        b = self.map['NDWORDS0']
+        s = []
+        for i in range(4):
+            r = self.read(b)
+            if verbose:
+                print(f'TURFIO{i} : {4*r} bytes received')
+            s.append(4*r)
+        return s
+
+    def reset(self):
+        rv = bf(self.read(0))
+        rv[0] = 1
+        self.write(0, int(rv))
+        rv[0] = 0
+        self.write(0, int(rv))
+
+    @property
+    def mask(self):
+        rv = bf(self.read(0))
+        return rv[12:8]
+
+    @mask.setter
+    def mask(self, value):
+        value = value & 0xF
+        rv = bf(self.read(0))
+        rv[12:8] = value
+        self.write(0, int(rv))
