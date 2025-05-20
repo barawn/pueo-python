efe0736a15afb83a7a26a66b6fc9039b35b93f2f
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-05-20 17:28:14 -0400

    refactor using the new Uploader module

--- pueo/turfio/surfturf.py
+++ pueo/turfio/surfturf.py
@@ -1,6 +1,6 @@
 from ..common.bf import bf
 from ..common.dev_submod import dev_submod
-
+from ..common.uploader import Uploader
 import os
 import struct
 
@@ -18,6 +18,8 @@ class SURFTURF(dev_submod):
     
     def __init__(self, dev, base):
         super().__init__(dev, base)
+        self.uploader = Uploader(self.fwupd,
+                                 self.mark)
 
     @property
     def autotrain(self):
@@ -67,69 +69,6 @@ class SURFTURF(dev_submod):
         while rv[9:8]:
             rv = bf(self.read(0x0))
             
-    @staticmethod
-    def fwupdHeader(fn):
-        hdr = bytearray(b'PYFW')
-        flen = os.path.getsize(fn)
-        hdr += struct.pack(">I", flen)
-        hdr += fn.encode()
-        hdr += b'\x00'
-        hdr += (256 - (sum(hdr) % 256)).to_bytes(1, 'big')
-        return (hdr, flen)
-
-    # upload through the SURFTURF common.
-    # probably not the smart way to do it.
-    # this already assumes you've put the SURF in
-    # eDownloadMode (which means addr(0xC)[7] = 1)
-    # returns the bank that it ended up in.
-    # "surf" here can be an individual SURF or all of 'em
-    # if you want to be pedantic.
-    def upload(self, surf, fn, destfn=None, bank=0, verbose=False):
-        if not isinstance(surf, list):
-            surf = [surf]
-        if not destfn:
-            destfn = fn
-        # ALL OF THIS could be done ahead of time, like you
-        # literally break the entire file up into bank chunks.
-        # But whatever. We'll see. Through the TURFIO
-        # this is obviously going to be slow.
-        if not os.path.isfile(fn):
-            raise ValueError("%s is not a regular file" % fn)
-        hdr, flen = self.fwupdHeader(destfn)
-        toRead = self.BANKLEN - len(hdr)
-        toRead = flen if flen < toRead else toRead
-        print("Uploading %s to %s" % (fn, destfn))
-        print("Header is %d bytes, reading %d bytes from file" % (len(hdr), toRead))
-        # these are here to make the loop work
-        d = hdr
-        written = 0
-        with open(fn, "rb") as f:
-            while written < flen:
-                if verbose:
-                    print("%s -> %s : writing %d bytes into bank %d, %d/%d written" %
-                          (fn, destfn, toRead, bank, written, flen))
-                d += f.read(toRead)
-                padBytes = (4-(len(d) % 4)) if (len(d) % 4) else 0
-                d += padBytes*b'\x00'
-                fmt = ">%dI" % (len(d) // 4)
-                il = struct.unpack(fmt, d)
-                # check to see if that bank is ready
-                testIdx = bank + 14
-                for s in surf:
-                    rv = bf(s.read(0xC))
-                    while not rv[testIdx]:
-                        rv = bf(s.read(0xC))
-                for val in il:
-                    self.fwupd(val)
-                self.mark(bank)
-                bank = bank ^ 1
-                written += toRead
-                remain = flen - written
-                toRead = remain if remain < self.BANKLEN else self.BANKLEN
-                # empty d b/c we add to it above
-                d = b''
-        return bank
-    
     # need to add runmode/trigger            
     def fwupd(self, val):
         self.write(0x4, val)
