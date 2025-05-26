aac21becd7477b6d3fbde3486ea1d23675349add
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-05-25 20:26:05 -0400

    add cin offset param

--- pueo/turf/pueo_turfif.py
+++ pueo/turf/pueo_turfif.py
@@ -16,6 +16,16 @@ class PueoTURFIF(dev_submod):
         for i in range(8):
             self.bit.append(PueoTURFIOBit(dev, base + 0x800 + 0x100*i))
 
+    @property
+    def cin_offset(self):
+        return (self.read(0) >> 8) & 0x7
+
+    @cin_offset.setter
+    def cin_offset(self, value):
+        r = self.read(0) & 0xFFFFF8FF
+        r |= (value & 0x7) << 8
+        self.write(0, r)
+            
     def train_enable(self, enable):
         rv = bf(self.read(self.map['CONTROL']))
         if enable:
