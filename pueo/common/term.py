0aeb1c70b699e05c64027b4e62c67e41668995fe
Patrick Allison <allison.122@osu.edu>
2025-06-05 16:44:21 +0000

    add more term defs

--- pueo/common/term.py
+++ pueo/common/term.py
@@ -8,8 +8,21 @@ class Term:
     GREEN = '\033[92m'
     YELLOW = '\033[93m'
     RED = '\033[91m'
+    BLACK = '\033[30m'
+    WHITE = '\033[37m'
+
+    BG_BLACK = '\033[40m'
+    BG_RED = '\033[41m'
+    BG_GREEN = '\033[42m'
+    BG_YELLOW = '\033[43m'
+    BG_BLUE = '\033[44m'
+    BG_MAGENTA = '\033[45m'
+    BG_CYAN = '\033[46m'
+    BG_WHITE = '\033[47m'    
+    
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+    REVERSE = '\033[7m'
     END = '\033[0m'
 
     CURSOR_POS = lambda x, y : f'\033[{x};{y}f'
