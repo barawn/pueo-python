aa619814c4705fd281e1d7ca74e8c087de4dfeab
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-06-20 00:41:28 -0400

    add frightening functions for signed support

--- pueo/common/dev_submod.py
+++ pueo/common/dev_submod.py
@@ -4,26 +4,30 @@
 # This entire file should probably be named something else.
 
 # property helper
-def bitfield(address, start, mask, doc=""): 
+def bitfield(address, start, mask, doc="", signed=False): 
     """ Creates a bitfield property: a value at 'address' starting at bit 'start' with bits marked in 'mask' """
-    return property(fget=lambda self : (self.read(address) >> start) & mask,
+    twid = ((mask+1)>>1)*int(signed)
+    return property(fget=lambda self : (((self.read(address) >> start) & mask) ^ twid)-twid,
                     fset=lambda self, v : (self.write(address, (self.read(address) & (~(mask<<start))) | ((int(v)&mask)<<start))),
                     doc=doc)
 
-def bitfield_ro(address, start, mask, doc=""):
+def bitfield_ro(address, start, mask, doc="", signed=False):
     """ Read-only bitfield property: a value at 'address' starting at bit 'start' with bits marked in 'mask' """
-    return property(fget=lambda self : (self.read(address) >> start) & mask,
+    twid = ((mask+1)>>1)*int(signed)
+    return property(fget=lambda self : (((self.read(address) >> start) & mask) ^ twid)-twid,
                     doc=doc)
     
-def register(address, doc=""):
+def register(address, doc="", signed=False):
     """ Creates a register property: a 32-bit value at 'address' """
-    return property(fget=lambda self : self.read(address),
+    twid = 0x80000000 if signed else 0
+    return property(fget=lambda self : (self.read(address) ^ twid) - twid,
                     fset=lambda self, v : self.write(address, int(v)),
                     doc=doc)
 
-def register_ro(address, doc=""):
+def register_ro(address, doc="", signed=False):
     """ Read only register property: a 32-bit value at 'address' """
-    return property(fget=lambda self : self.read(address),
+    twid = 0x80000000 if signed else 0    
+    return property(fget=lambda self : (self.read(address) ^ twid) - twid,
                     doc=doc)
 
 class dev_submod:
